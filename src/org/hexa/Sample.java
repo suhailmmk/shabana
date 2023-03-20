a1dd2dbe2ed73af4bb0aac1dd276fc61ce93743d
mohasuhail <suhailyas2023@gmail.com>
2023-03-20 16:17:09 +0530

    hisuhail

---
+++ src/org/hexa/Sample.java
@@ -0,0 +1,12 @@
+package org.hexa;
+
+public class Sample {
+  private void method1() {
+	// TODO Auto-generated method stub
+
+}
+  private void method2() {
+	// TODO Auto-generated method stub
+
+}
+}
